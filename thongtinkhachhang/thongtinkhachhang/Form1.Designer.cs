63ad4086ae5a9df343e4f2c142b4f3626b399335
vixidurr2402 <chien27122402@gmail.com>
2024-05-30 01:26:50 +0700

    codemaiktra

---
+++ thongtinkhachhang/thongtinkhachhang/Form1.Designer.cs
@@ -0,0 +1,276 @@
+﻿namespace thongtinkhachhang
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnShow = new System.Windows.Forms.Button();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnEdit = new System.Windows.Forms.Button();
+            this.bntDel = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.txtId = new System.Windows.Forms.TextBox();
+            this.tenkh = new System.Windows.Forms.TextBox();
+            this.dc = new System.Windows.Forms.TextBox();
+            this.dt = new System.Windows.Forms.TextBox();
+            this.email = new System.Windows.Forms.TextBox();
+            this.dgvCustomer = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.search = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvCustomer)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnShow
+            // 
+            this.btnShow.Location = new System.Drawing.Point(95, 214);
+            this.btnShow.Name = "btnShow";
+            this.btnShow.Size = new System.Drawing.Size(75, 23);
+            this.btnShow.TabIndex = 0;
+            this.btnShow.Text = "Hiển thị";
+            this.btnShow.UseVisualStyleBackColor = true;
+            this.btnShow.Click += new System.EventHandler(this.btnShow_Click);
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(186, 214);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(75, 23);
+            this.btnAdd.TabIndex = 0;
+            this.btnAdd.Text = "Thêm";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnEdit
+            // 
+            this.btnEdit.Location = new System.Drawing.Point(267, 214);
+            this.btnEdit.Name = "btnEdit";
+            this.btnEdit.Size = new System.Drawing.Size(75, 23);
+            this.btnEdit.TabIndex = 0;
+            this.btnEdit.Text = "Sửa";
+            this.btnEdit.UseVisualStyleBackColor = true;
+            this.btnEdit.Click += new System.EventHandler(this.btnEdit_Click);
+            // 
+            // bntDel
+            // 
+            this.bntDel.Location = new System.Drawing.Point(348, 214);
+            this.bntDel.Name = "bntDel";
+            this.bntDel.Size = new System.Drawing.Size(75, 23);
+            this.bntDel.TabIndex = 0;
+            this.bntDel.Text = "Xoá";
+            this.bntDel.UseVisualStyleBackColor = true;
+            this.bntDel.Click += new System.EventHandler(this.bntDel_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(429, 214);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 23);
+            this.btnExit.TabIndex = 0;
+            this.btnExit.Text = "Thoát";
+            this.btnExit.UseVisualStyleBackColor = true;
+            // 
+            // txtId
+            // 
+            this.txtId.Location = new System.Drawing.Point(116, 82);
+            this.txtId.Name = "txtId";
+            this.txtId.Size = new System.Drawing.Size(166, 20);
+            this.txtId.TabIndex = 1;
+            // 
+            // tenkh
+            // 
+            this.tenkh.Location = new System.Drawing.Point(116, 129);
+            this.tenkh.Name = "tenkh";
+            this.tenkh.Size = new System.Drawing.Size(166, 20);
+            this.tenkh.TabIndex = 1;
+            // 
+            // dc
+            // 
+            this.dc.Location = new System.Drawing.Point(116, 171);
+            this.dc.Name = "dc";
+            this.dc.Size = new System.Drawing.Size(166, 20);
+            this.dc.TabIndex = 1;
+            // 
+            // dt
+            // 
+            this.dt.Location = new System.Drawing.Point(326, 82);
+            this.dt.Name = "dt";
+            this.dt.Size = new System.Drawing.Size(166, 20);
+            this.dt.TabIndex = 1;
+            // 
+            // email
+            // 
+            this.email.Location = new System.Drawing.Point(326, 129);
+            this.email.Name = "email";
+            this.email.Size = new System.Drawing.Size(166, 20);
+            this.email.TabIndex = 1;
+            // 
+            // dgvCustomer
+            // 
+            this.dgvCustomer.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgvCustomer.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
+            this.dgvCustomer.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvCustomer.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.dgvCustomer.Location = new System.Drawing.Point(0, 251);
+            this.dgvCustomer.Name = "dgvCustomer";
+            this.dgvCustomer.Size = new System.Drawing.Size(611, 199);
+            this.dgvCustomer.TabIndex = 2;
+            this.dgvCustomer.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvCustomer_CellClick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(116, 63);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(81, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "ID Khách Hàng";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(116, 113);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(89, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Tên Khách Hàng";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(323, 63);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(59, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Điện Thoại";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(323, 113);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(32, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Email";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(116, 155);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(41, 13);
+            this.label5.TabIndex = 3;
+            this.label5.Text = "Địa Chỉ";
+            // 
+            // search
+            // 
+            this.search.Location = new System.Drawing.Point(326, 171);
+            this.search.Name = "search";
+            this.search.Size = new System.Drawing.Size(166, 20);
+            this.search.TabIndex = 1;
+            this.search.TextChanged += new System.EventHandler(this.search_TextChanged);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(323, 152);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(91, 13);
+            this.label6.TabIndex = 3;
+            this.label6.Text = "Tìm kiếm theo tên";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(162, 19);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(291, 25);
+            this.label8.TabIndex = 4;
+            this.label8.Text = "THÔNG TIN KHÁCH HÀNG";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(611, 450);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dgvCustomer);
+            this.Controls.Add(this.email);
+            this.Controls.Add(this.search);
+            this.Controls.Add(this.dc);
+            this.Controls.Add(this.dt);
+            this.Controls.Add(this.tenkh);
+            this.Controls.Add(this.txtId);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.bntDel);
+            this.Controls.Add(this.btnEdit);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.btnShow);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvCustomer)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnShow;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnEdit;
+        private System.Windows.Forms.Button bntDel;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.TextBox txtId;
+        private System.Windows.Forms.TextBox tenkh;
+        private System.Windows.Forms.TextBox dc;
+        private System.Windows.Forms.TextBox dt;
+        private System.Windows.Forms.TextBox email;
+        private System.Windows.Forms.DataGridView dgvCustomer;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox search;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label8;
+    }
+}
+
---
+++ thongtinkhachhang/thongtinkhachhang/Form1.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+namespace thongtinkhachhang
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private string connectionString = @"Data Source=.\sqlexpress;Initial Catalog=thongtinkhachhang;Integrated Security=True;";
+
+        private void btnShow_Click(object sender, EventArgs e)
+        {
+            SqlConnection conn = new SqlConnection(connectionString);
+            conn.Open();
+            string showquery = "SELECT * FROM customer ORDER BY id DESC";
+            SqlDataAdapter sda = new SqlDataAdapter(showquery, conn);
+            DataTable dt = new DataTable();
+            sda.Fill(dt);
+            dgvCustomer.DataSource = dt;
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            SqlConnection conn = new SqlConnection(connectionString);
+            string query = "INSERT INTO customer VALUES (@id, @ten, @diachi, @dt, @email)";
+            conn.Open();
+            SqlCommand cmd = new SqlCommand(query, conn);
+            cmd.Parameters.AddWithValue("@id", txtId.Text);
+            cmd.Parameters.AddWithValue("@ten", tenkh.Text);
+            cmd.Parameters.AddWithValue("@diachi", dc.Text);
+            cmd.Parameters.AddWithValue("@dt", dt.Text);
+            cmd.Parameters.AddWithValue("@email", email.Text);
+            cmd.ExecuteNonQuery();
+            btnShow_Click(sender, e);
+        }
+
+        private void bntDel_Click(object sender, EventArgs e)
+        {
+            SqlConnection conn = new SqlConnection(connectionString);
+            conn.Open();
+            int rowIndex = dgvCustomer.SelectedCells[0].RowIndex;
+            string ma = dgvCustomer.Rows[rowIndex].Cells[0].Value.ToString();
+            string deleteQuery = "DELETE FROM customer WHERE id = @id";
+            SqlCommand cmd = new SqlCommand(deleteQuery, conn);
+            cmd.Parameters.AddWithValue("@id", ma);
+            cmd.ExecuteNonQuery();
+            btnShow_Click(sender, e);
+        }
+
+        private void dgvCustomer_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            DataGridViewRow row = dgvCustomer.Rows[e.RowIndex];
+            txtId.Text = row.Cells[0].Value.ToString();
+            tenkh.Text = row.Cells[1].Value.ToString();
+            dc.Text = row.Cells[2].Value.ToString();
+            dt.Text = row.Cells[3].Value.ToString();
+            email.Text = row.Cells[4].Value.ToString();
+        }
+
+        private void btnEdit_Click(object sender, EventArgs e)
+        {
+            SqlConnection connection = new SqlConnection(connectionString);
+            SqlCommand cmd = connection.CreateCommand();
+            connection.Open();
+            cmd.CommandText = "UPDATE customer SET tenkh = @ten, diachi = @dc, dienthoai = @dt, email = @email WHERE id = @id";
+            cmd.Parameters.AddWithValue("@ten", tenkh.Text);
+            cmd.Parameters.AddWithValue("@dc", dc.Text);
+            cmd.Parameters.AddWithValue("@dt", dt.Text);
+            cmd.Parameters.AddWithValue("@email", email.Text);
+            cmd.Parameters.AddWithValue("@id", txtId.Text);
+            cmd.ExecuteNonQuery();
+            btnShow_Click(sender, e);
+        }
+
+        private void search_TextChanged(object sender, EventArgs e)
+        {
+            SqlConnection conn = new SqlConnection(connectionString);
+            string searchQuery = "SELECT * FROM customer WHERE tenkh LIKE @tenkh"; 
+            conn.Open();
+            SqlCommand cmd = new SqlCommand(searchQuery, conn);
+            cmd.Parameters.AddWithValue("@tenkh", "%"+ search.Text+ "%");
+            SqlDataAdapter sda = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            sda.Fill(dt);
+            dgvCustomer.DataSource = dt;
+        }
+    }
+}
